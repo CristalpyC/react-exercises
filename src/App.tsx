103cdefa51952c5131be6e7473ea372ffa67d82f
cristalPyC <cristaltn03@gmail.com>
2024-07-30 16:47:25 -0400

    flushSync

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import './App.css'
-import { Counter } from './components/render-props/Counter';
+import { FlusBtn } from './components/FlushSync/FlushBtn';
 
 function App() {
   return (
    <>
     <h1 className='mb-4'>Counter: Render props</h1>
-    <Counter>
-      {(sum, sumNum) => (
-       <div>
-         <p>Num: {sum}</p>
-         <button className='text-black p-2 mt-4 bg-blue-100 hover:bg-blue-300' onClick={sumNum}>+ 2</button>
-       </div>
-      )}
-    </Counter>
-   </>
+    <FlusBtn />
+  </>
   );
 }
 
